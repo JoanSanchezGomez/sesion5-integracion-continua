c11a9726ab7ffca374e36faf55da123f887ef9ee
Joan Sanchez Gomez <128400051+JoanSanchezGomez@users.noreply.github.com>
2023-04-16 14:42:17 +0200

    Update test3.js

--- test/test3.js
+++ test/test3.js
@@ -6,7 +6,24 @@ var should = require('should');
 // Cargar el módulo con las funciones para testear
 var operaciones = require('../operations.js');
 
+
+describe('Función resta', function () {
+        it('Resta de números positivos', function () {
+            assert.equal(math.restar(4, 6), -2);
+            assert.equal(math.restar(6, 4), 2);
+            assert.equal(math.restar(6, 6), 0);
+        });
+        it('Resta de números positivos y negativos', function () {
+            assert.equal(math.restar(-2, 6), -8);
+        });
+        it('Resta de números negativos', function () {
+            assert.equal(math.restar(-2, -3), 1);
+        });
+    });
+
+
 // Test
+describe('Función par', function () {
 it('comprobar función par', function() {
   operaciones.par.should.be.a.Function();
   should.equal(operaciones.par(2), 0);
