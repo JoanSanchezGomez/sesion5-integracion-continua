f9b58785c6fe28c6f67a12ba7628b065ef1bf054
Joan Sanchez Gomez <128400051+JoanSanchezGomez@users.noreply.github.com>
2023-04-16 13:32:04 +0200

    Create test3.js

---
+++ test/test3.js
@@ -0,0 +1,15 @@
+// Ejemplo de test utilizando la librería should
+
+// Cargar la librería should
+var should = require('should');
+
+// Cargar el módulo con las funciones para testear
+var operaciones = require('../operations.js');
+
+// Test
+it('comprobar función par', function() {
+  operaciones.par.should.be.a.Function();
+  should.equal(operaciones.par(2), 0);
+  should.equal(operaciones.par(4), 0);
+  should.equal(operaciones.par(5), 1);
+});
